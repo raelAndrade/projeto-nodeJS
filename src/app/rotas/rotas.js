f418bb122182eb73b84d14fbd96dc0076eb7cddd
raelAndrade <israelg.andrade@bol.com.br>
2020-08-24 01:38:49 -0300

    ADICIONANDO PADRÃO DAO

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,4 +1,5 @@
 const db = require('../../config/database');
+const LivroDao = require('../infra/livro-dao');
 
 module.exports = (app) => {
 
@@ -16,7 +17,8 @@ module.exports = (app) => {
     });
 
     app.get('/livros', function(req, resp){
-        db.all('SELECT * FROM livros', function(erro, resultados) {
+        const livroDao = new LivroDao(db);
+        livroDao.lista(function(erro, resultados){
             resp.marko(
                 require('../views/livros/lista/lista.marko'),
                 {
@@ -24,6 +26,5 @@ module.exports = (app) => {
                 }
             );
         });
-    
     });
 }
