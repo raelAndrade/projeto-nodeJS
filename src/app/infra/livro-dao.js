6166e714888d2a0e1de671e8131cb05e5e7829df
raelAndrade <israelg.andrade@bol.com.br>
2020-09-01 18:42:19 -0300

    Adicionando DAO, layout lista e formulario, rotas

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -1,13 +1,110 @@
+// Essa classe é responsável por disponibilizar toda e qualquer funcionalidade de acesso ao Banco de dados relativos ao Livro
 class LivroDao {
+
     constructor(db){
         this._db = db;
     }
 
-    lista(callback){
-        this._db.all(
-            'SELECT * FROM livros',
-            (erro, resultados) => callback(erro, resultados)
-        );
+    lista(){
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if(erro) return reject('Não foi possível listar os livros');
+
+                    return resolve(resultados);
+                }
+            )
+        });
+    }
+
+    adiciona(livro){
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                        titulo,
+                        preco,
+                        descricao
+                    ) values (?, ?, ?)
+                `, 
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ], 
+                function(err){
+                    if(err){
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+                    resolve();
+                }
+            );
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros 
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if(erro){
+                        console.log(erro);
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
+        });
+    }
+
+    buscaPorId(id){
+        return new Promise((resolve, reject) => {
+            this._db.get(`
+                SELECT * 
+                FROM livros 
+                WHERE id = ?
+            `,
+            [
+                id
+            ],
+            (err, livro) => {
+                if(err){
+                    console.log(err);
+                    return reject('Não possível encontrar o livro!');
+                }
+                return resolve(livro);
+            });
+        });
+    }
+
+    atualiza(livro){
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE LIVROS SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            (err) => {
+                if(err){
+                    return reject('Não foi possível atualizar o livro!');
+                }
+                resolve();
+            });
+        });
     }
 }
 
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,11 +1,29 @@
+require('marko/node-require').install();
+require('marko/express');
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use('/estatico', express.static('src/app/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // Middleware
+
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 
 const rotas = require('../app/rotas/rotas.js');
 rotas(app);
 
-require('marko/node-require').install();
-require('marko/express');
 
 
 module.exports = app;
